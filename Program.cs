861fb113bf8226f0f7be6a45a98660131bdf6416
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-03 16:38:15 +0100

    search for a term in multiple feeds in parallel

--- Program.cs
+++ Program.cs
@@ -1,36 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
-using NuGet.Versioning;
 
 namespace nugex
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static readonly string[] KnownFeeds = {
+            "https://apollo.healthcare.siemens.com/tfs/IKM.TPC.Projects/_packaging/syngo-BuildTools/nuget/v3/index.json",
+            "https://apollo.healthcare.siemens.com/tfs/IKM.TPC.Projects/_packaging/syngo-BuildIntegration-Extern/nuget/v3/index.json",
+            "https://apollo.healthcare.siemens.com/tfs/IKM.TPC.Projects/_packaging/syngo-Extern/nuget/v3/index.json",
+            "https://apollo.healthcare.siemens.com/tfs/IKM.TPC.Projects/_packaging/x-juba-upload-test/nuget/v3/index.json",
+            "https://api.nuget.org/v3/index.json"
+        };
+
+        static void Main(string[] args)
         {
             ILogger logger = NullLogger.Instance;
             CancellationToken cancellationToken = CancellationToken.None;
 
-            var feedUrl = "https://apollo.healthcare.siemens.com/tfs/IKM.TPC.Projects/_packaging/syngo-BuildTools/nuget/v3/index.json";
-
-            SourceCacheContext cache = new SourceCacheContext();
-            SourceRepository repository = Repository.Factory.GetCoreV3(feedUrl);
-
-            var searcher = await repository.GetResourceAsync<PackageSearchResource>();
-            var packages = await searcher.SearchAsync("microsoft", new SearchFilter(false), 0, 999, logger, cancellationToken);
-            var releaseVersionRule = new Regex(@"^\d+\.\d+.\d+$");
-
-            packages.ToList().ForEach(p =>
-            {
-                Console.WriteLine($"{p.Identity.Id} - {p.Identity.Version}");
+            var feedCrowlers = KnownFeeds.Select(feedUrl => FeedCrawler(feedUrl)).ToArray();
+            Task.WaitAll(feedCrowlers);
+            feedCrowlers.ToList().ForEach(feed => {
+                Console.WriteLine("--------");
+                feed.Result.ForEach(item => Console.WriteLine($"{item.Identity.Id} - {item.Identity.Version.ToString()}"));
             });
         }
+
+        static async Task<List<IPackageSearchMetadata>> FeedCrawler(string feedUrl)
+        {
+            SourceCacheContext cache = new SourceCacheContext();
+            SourceRepository repository = Repository.Factory.GetCoreV3(feedUrl);
+            var searcher = await repository.GetResourceAsync<PackageSearchResource>();
+            var packages = await searcher.SearchAsync(@"sy.m", new SearchFilter(false), 0, 999, NullLogger.Instance, CancellationToken.None);
+            return packages.ToList();
+        }
+
     }
 }
