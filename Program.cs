d533265633a4022c783b67111328659ba6691b6e
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-07 08:27:07 +0100

    extract command line processing into separate class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,20 +6,19 @@ using NuGet.Common;
 
 namespace nugex
 {
-    class Program
+    partial class Program
     {
-        private static List<string> Args;
-
         static void Main(string[] args)
         {
-            Args = new List<string>(args);
+            CmdLine.Parse(args);
+
             ILogger logger = NullLogger.Instance;
             CancellationToken cancellationToken = CancellationToken.None;
 
-            var searchTerm = Param("name");
+            var searchTerm = CmdLine.GetParam("name");
             if (string.IsNullOrWhiteSpace(searchTerm)) throw new Exception($"please use the --name parameter to specify a search term");
-            var showAllFeeds = Switch("show-all-feeds");
-            var includePreRelease = Switch("include-pre-release");
+            var showAllFeeds = CmdLine.GetSwitch("show-all-feeds");
+            var includePreRelease = CmdLine.GetSwitch("include-pre-release");
 
             var knownFeeds = new ConfigReader().ReadSources();
             var feedCrowlers = knownFeeds.Select(feed => new FeedCrawler(feed.Item1, feed.Item2)).ToList();
@@ -32,23 +30,5 @@ namespace nugex
                 crawler.Results.ToList().ForEach(item => Console.WriteLine($"{item.Identity.Id} - {item.Identity.Version.ToString()}"));
             });
         }
-
-        static int FindOption(string name)
-        {
-            var arg = Args
-                .LastOrDefault(a => a.Trim(new[] { '/', '-' }).ToLowerInvariant().Equals(name, StringComparison.InvariantCultureIgnoreCase));
-            if (arg == default) return -1;
-            return Args.IndexOf(arg);
-        }
-
-        static string Param(string name)
-        {
-            var valueIdx = FindOption(name) + 1;
-            if (valueIdx == 0 || valueIdx >= Args.Count) return default;
-            return Args[valueIdx];
-        }
-        
-        static bool Switch(string name) => FindOption(name) >= 0;
-
     }
 }
