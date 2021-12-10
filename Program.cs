9405551de1a1ce197a6e11fa0d2a70437749c88c
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-10 08:29:22 +0100

    introduce the 'download' command

--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@ namespace nugex
                         new Parameter(_VSPEC_, "version number (regex)"),
                         new Switch(_ALL_FEEDS_, "...even those without matching packages")
                     ),
+                new Command("download", "... a package from nuget.org", () => Download(),
+                        new Parameter(_SEARCH_TERM_, "search term (^ and $ can be used to make the phrase more specific)", mandatory: true),
+                        new Parameter(_VSPEC_, "version number (regex)", mandatory: true),
+                        new Parameter(_TARGET_PATH_, "download folder for the package")
+                    )
             });
 
             CmdLine.Parser.Parse(args);
@@ -22,6 +27,7 @@ namespace nugex
         // option names in alphabetical order
         public static readonly string _SEARCH_TERM_ = "name";
         public static readonly string _ALL_FEEDS_ = "show-all-feeds";
+        public static readonly string _TARGET_PATH_ = "targetDir";
         public static readonly string _VSPEC_ = "version";
 
     }
---
+++ commands/Download.cs
@@ -0,0 +1,45 @@
+using NuGet.Common;
+using NuGet.Protocol.Core.Types;
+using NuGet.Protocol;
+using NuGet.Versioning;
+using nugex.cmdline;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace nugex
+{
+    partial class Program
+    {
+        private static void Download() {
+            var packageName = CmdLine.Parser.GetParam(_SEARCH_TERM_);
+            if (string.IsNullOrWhiteSpace(packageName)) throw new Exception($"please use the {_SEARCH_TERM_} parameter to specify the package");
+            var versionSpec = CmdLine.Parser.GetParam(_VSPEC_);
+            if (string.IsNullOrWhiteSpace(versionSpec)) throw new Exception($"please use the {_VSPEC_} parameter to specify the version");
+            var targetDirectory = CmdLine.Parser.GetParam(_TARGET_PATH_);
+            if (string.IsNullOrWhiteSpace(targetDirectory)) targetDirectory = Environment.ExpandEnvironmentVariables($"%TEMP%\\{Guid.NewGuid()}");
+
+            var filePath = DownloadAsync(packageName, versionSpec, targetDirectory).Result;
+            System.Console.WriteLine($"download completed: {filePath}");
+        }
+
+        private static async Task<string> DownloadAsync(string packageName, string version, string targetDirectory)
+        {
+            var cache = new SourceCacheContext();
+            var repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+            var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+            var fullPath = Path.Join(targetDirectory, $"{packageName}.{version}.nupkg");
+            using FileStream fileStream = new FileStream(fullPath, FileMode.CreateNew);
+            await resource.CopyNupkgToStreamAsync(
+                packageName,
+                new NuGetVersion(version),
+                fileStream,
+                cache,
+                NullLogger.Instance,
+                CancellationToken.None);
+            return fullPath;
+        }
+    }
+}
