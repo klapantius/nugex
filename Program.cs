5bf9c06356cf4341aa70e13c768145160dae783e
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2021-12-12 20:25:42 +0100

    copy a package from nuget to an interal feed

--- Program.cs
+++ Program.cs
@@ -13,9 +13,15 @@ namespace nugex
                         new Switch(_ALL_FEEDS_, "...even those without matching packages")
                     ),
                 new Command("download", "... a package from nuget.org", () => Download(),
-                        new Parameter(_SEARCH_TERM_, "search term (^ and $ can be used to make the phrase more specific)", mandatory: true),
-                        new Parameter(_VSPEC_, "version number (regex)", mandatory: true),
+                        new Parameter(_SEARCH_TERM_, "exact package id", mandatory: true),
+                        new Parameter(_VSPEC_, "exact version number", mandatory: true),
                         new Parameter(_TARGET_PATH_, "download folder for the package")
+                    ),
+                new Command("copy", "... a package from nuget.org", () => Copy(),
+                        new Parameter(_SEARCH_TERM_, "exact package id", mandatory: true),
+                        new Parameter(_VSPEC_, "exact version number", mandatory: true),
+                        new Parameter(_TARGET_FEED_, "the internal location for the package", mandatory: true),
+                        new Parameter(_API_KEY_, "for the case the default value would not work")
                     )
             });
 
@@ -25,8 +31,10 @@ namespace nugex
         }
 
         // option names in alphabetical order
+        public static readonly string _API_KEY_ = "apiKey";
         public static readonly string _SEARCH_TERM_ = "name";
         public static readonly string _ALL_FEEDS_ = "show-all-feeds";
+        public static readonly string _TARGET_FEED_ = "targetFeed";
         public static readonly string _TARGET_PATH_ = "targetDir";
         public static readonly string _VSPEC_ = "version";
 
---
+++ commands/Copy.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using nugex.cmdline;
+using nugex.utils;
+
+namespace nugex
+{
+    partial class Program
+    {
+        private static void Copy()
+        {
+            var packageName = CmdLine.Parser.GetParam(_SEARCH_TERM_);
+            if (string.IsNullOrWhiteSpace(packageName)) throw new Exception($"please use the {_SEARCH_TERM_} parameter to specify the package");
+            var versionSpec = CmdLine.Parser.GetParam(_VSPEC_);
+            if (string.IsNullOrWhiteSpace(versionSpec)) throw new Exception($"please use the {_VSPEC_} parameter to specify the version");
+            var targetFeed = CmdLine.Parser.GetParam(_TARGET_FEED_);
+            if (string.IsNullOrWhiteSpace(targetFeed)) throw new Exception($"please use the {_TARGET_FEED_} parameter to specify the target feed");
+            var apiKey = CmdLine.Parser.GetParam(_API_KEY_);
+
+            CopyAsync(packageName, versionSpec, targetFeed).Wait();
+            System.Console.WriteLine($"transfer completed");
+        }
+        
+        private static async Task CopyAsync(string packageName, string version, string targetFeed, string apiKey = "AzureDevOps")
+        {
+            var transferFolder = EnsureDownloadFolder(null);
+            var package = await DownloadAsync(packageName, version, transferFolder);
+
+            var config = new ConfigReader();
+            var url = config.GetUrlOfFeed(targetFeed);
+            var repository = Repository.Factory.GetCoreV3(url);
+            var uploader = await repository.GetResourceAsync<PackageUpdateResource>();
+
+            await uploader.Push(
+                new[] { package },
+                symbolSource: null,
+                timeoutInSecond: 4 * 15,
+                disableBuffering: false,
+                getApiKey: packageSource => apiKey,
+                getSymbolApiKey: packageSource => null,
+                noServiceEndpoint: false,
+                skipDuplicate: false,
+                symbolPackageUpdateResource: null,
+                NullLogger.Instance);
+        }
+
+    }
+}
